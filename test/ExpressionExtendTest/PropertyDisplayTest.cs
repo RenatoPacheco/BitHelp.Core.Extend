4ca7b2fbaeb7c348c6588f11051c4a94d8fc8ab6
Renato Pacheco <renato.pacheco@adm.unip.br>
2022-07-28 21:59:53 -0300

    Set methods to static

--- test/ExpressionExtendTest/PropertyDisplayTest.cs
+++ test/ExpressionExtendTest/PropertyDisplayTest.cs
@@ -6,7 +6,7 @@ namespace BitHelp.Core.Extend.Test.ExpressionExtendTest
 {
     public class PropertyDisplayTest
     {
-        private string PropertyDisplay<T>(Expression<Func<T, object>> expression)
+        private static string PropertyDisplay<T>(Expression<Func<T, object>> expression)
         {
             return expression.PropertyDisplay();
         }
@@ -17,40 +17,40 @@ namespace BitHelp.Core.Extend.Test.ExpressionExtendTest
             SingleValues single = new SingleValues();
             ArrayValues array = new ArrayValues();
 
-            Assert.Equal(nameof(single.Char), this.PropertyDisplay<SingleValues>(x => x.Char));
-            Assert.Equal(nameof(single.String), this.PropertyDisplay<SingleValues>(x => x.String));
-            Assert.Equal(nameof(single.Int), this.PropertyDisplay<SingleValues>(x => x.Int));
-            Assert.Equal("Int null", this.PropertyDisplay<SingleValues>(x => x.IntNull));
-            Assert.Equal(nameof(single.Long), this.PropertyDisplay<SingleValues>(x => x.Long));
-            Assert.Equal("Long null", this.PropertyDisplay<SingleValues>(x => x.LongNull));
-            Assert.Equal(nameof(single.Decimal), this.PropertyDisplay<SingleValues>(x => x.Decimal));
-            Assert.Equal("Decimal null", this.PropertyDisplay<SingleValues>(x => x.DecimalNull));
-            Assert.Equal(nameof(single.Uint), this.PropertyDisplay<SingleValues>(x => x.Uint));
-            Assert.Equal("Uint null", this.PropertyDisplay<SingleValues>(x => x.UintNull));
-            Assert.Equal("Date time", this.PropertyDisplay<SingleValues>(x => x.DateTime));
-            Assert.Equal("Date time null", this.PropertyDisplay<SingleValues>(x => x.DateTimeNull));
-            Assert.Equal(nameof(single.Guid), this.PropertyDisplay<SingleValues>(x => x.Guid));
-            Assert.Equal("Guid null", this.PropertyDisplay<SingleValues>(x => x.GuidNull));
-            Assert.Equal(nameof(single.Bool), this.PropertyDisplay<SingleValues>(x => x.Bool));
-            Assert.Equal("Bool null", this.PropertyDisplay<SingleValues>(x => x.BoolNull));
+            Assert.Equal(nameof(single.Char), PropertyDisplay<SingleValues>(x => x.Char));
+            Assert.Equal(nameof(single.String), PropertyDisplay<SingleValues>(x => x.String));
+            Assert.Equal(nameof(single.Int), PropertyDisplay<SingleValues>(x => x.Int));
+            Assert.Equal("Int null", PropertyDisplay<SingleValues>(x => x.IntNull));
+            Assert.Equal(nameof(single.Long), PropertyDisplay<SingleValues>(x => x.Long));
+            Assert.Equal("Long null", PropertyDisplay<SingleValues>(x => x.LongNull));
+            Assert.Equal(nameof(single.Decimal), PropertyDisplay<SingleValues>(x => x.Decimal));
+            Assert.Equal("Decimal null", PropertyDisplay<SingleValues>(x => x.DecimalNull));
+            Assert.Equal(nameof(single.Uint), PropertyDisplay<SingleValues>(x => x.Uint));
+            Assert.Equal("Uint null", PropertyDisplay<SingleValues>(x => x.UintNull));
+            Assert.Equal("Date time", PropertyDisplay<SingleValues>(x => x.DateTime));
+            Assert.Equal("Date time null", PropertyDisplay<SingleValues>(x => x.DateTimeNull));
+            Assert.Equal(nameof(single.Guid), PropertyDisplay<SingleValues>(x => x.Guid));
+            Assert.Equal("Guid null", PropertyDisplay<SingleValues>(x => x.GuidNull));
+            Assert.Equal(nameof(single.Bool), PropertyDisplay<SingleValues>(x => x.Bool));
+            Assert.Equal("Bool null", PropertyDisplay<SingleValues>(x => x.BoolNull));
 
-            Assert.Equal("Array char", this.PropertyDisplay<ArrayValues>(x => x.Char));
-            Assert.Equal("Array string", this.PropertyDisplay<ArrayValues>(x => x.String));
-            Assert.Equal("Array int", this.PropertyDisplay<ArrayValues>(x => x.Int));
-            Assert.Equal("Array long", this.PropertyDisplay<ArrayValues>(x => x.Long));
-            Assert.Equal("Array decimal", this.PropertyDisplay<ArrayValues>(x => x.Decimal));
-            Assert.Equal("Array uint", this.PropertyDisplay<ArrayValues>(x => x.Uint));
-            Assert.Equal("Array date time", this.PropertyDisplay<ArrayValues>(x => x.DateTime));
-            Assert.Equal("Array guid", this.PropertyDisplay<ArrayValues>(x => x.Guid));
-            Assert.Equal("Array bool", this.PropertyDisplay<ArrayValues>(x => x.Bool));
+            Assert.Equal("Array char", PropertyDisplay<ArrayValues>(x => x.Char));
+            Assert.Equal("Array string", PropertyDisplay<ArrayValues>(x => x.String));
+            Assert.Equal("Array int", PropertyDisplay<ArrayValues>(x => x.Int));
+            Assert.Equal("Array long", PropertyDisplay<ArrayValues>(x => x.Long));
+            Assert.Equal("Array decimal", PropertyDisplay<ArrayValues>(x => x.Decimal));
+            Assert.Equal("Array uint", PropertyDisplay<ArrayValues>(x => x.Uint));
+            Assert.Equal("Array date time", PropertyDisplay<ArrayValues>(x => x.DateTime));
+            Assert.Equal("Array guid", PropertyDisplay<ArrayValues>(x => x.Guid));
+            Assert.Equal("Array bool", PropertyDisplay<ArrayValues>(x => x.Bool));
         }
 
         [Fact]
         public void Check_property_display_in_subclass()
         {
-            Assert.Equal("String", this.PropertyDisplay<AllValues>(x => x.SingleValues.String));
-            Assert.Equal("Int null", this.PropertyDisplay<AllValues>(x => x.SingleValues.IntNull));
-            Assert.Equal("Array string", this.PropertyDisplay<AllValues>(x => x.ArrayValues.String));
+            Assert.Equal("String", PropertyDisplay<AllValues>(x => x.SingleValues.String));
+            Assert.Equal("Int null", PropertyDisplay<AllValues>(x => x.SingleValues.IntNull));
+            Assert.Equal("Array string", PropertyDisplay<AllValues>(x => x.ArrayValues.String));
         }
     }
 }
