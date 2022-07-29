11aac25653b39722ceb750d4a6f518795ca6c309
Renato Pacheco <renato.pacheco@adm.unip.br>
2022-07-29 08:42:37 -0300

    Adding     public class PropertyDescriptionTest

--- test/SingleValues.cs
+++ test/SingleValues.cs
@@ -1,29 +1,36 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace BitHelp.Core.Extend.Test
 {
     public class SingleValues
     {
+        [Description("Char description")]
         public char Char { get; set; }
 
+        [Description("String description")]
         public string String { get; set; }
 
+        [Description("Int description")]
         public int Int { get; set; }
 
         [Display(Name = "Int null")]
         public int? IntNull { get; set; }
 
+        [Description("Long description")]
         public long Long { get; set; }
 
         [Display(Name = "Long null")]
         public long? LongNull { get; set; }
 
+        [Description("Decimal description")]
         public decimal Decimal { get; set; }
 
         [Display(Name = "Decimal null")]
         public decimal? DecimalNull { get; set; }
 
+        [Description("Uint description")]
         public uint Uint { get; set; }
 
         [Display(Name = "Uint null")]
@@ -35,11 +42,13 @@ namespace BitHelp.Core.Extend.Test
         [Display(Name = "Date time null")]
         public DateTime? DateTimeNull { get; set; }
 
+        [Description("Guid description")]
         public Guid Guid { get; set; }
 
         [Display(Name = "Guid null")]
         public Guid? GuidNull { get; set; }
 
+        [Description("Bool description")]
         public bool Bool { get; set; }
 
         [Display(Name = "Bool null")]
