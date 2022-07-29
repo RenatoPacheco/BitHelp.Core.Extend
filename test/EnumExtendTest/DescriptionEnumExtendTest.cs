30103d486cd88046cfa71fc99b405eabb3a0c055
Renato Pacheco <renato.pacheco@adm.unip.br>
2022-07-29 10:08:53 -0300

    Adding EnumExtendTest

---
+++ test/EnumExtendTest/DescriptionEnumExtendTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace BitHelp.Core.Extend.Test.EnumExtendTest
+{
+    public class DescriptionEnumExtendTest
+    {
+        [Fact]
+        public void Check_contain_description()
+        {
+            Enum value = EnumValue.Two;
+
+            Assert.Equal("Two description", value.Description());
+        }
+
+        [Fact]
+        public void Check_not_contain_description()
+        {
+            Enum value = EnumValue.Three;
+
+            Assert.Null(value.Description());
+        }
+    }
+}
---
+++ test/EnumExtendTest/DisplayEnumExtendTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace BitHelp.Core.Extend.Test.EnumExtendTest
+{
+    public class DisplayEnumExtendTest
+    {
+        [Fact]
+        public void Check_contain_display()
+        {
+            Enum value = EnumValue.One;
+
+            Assert.Equal("One name", value.Display());
+        }
+
+        [Fact]
+        public void Check_not_contain_display()
+        {
+            Enum value = EnumValue.Three;
+
+            Assert.Equal("Three", value.Display());
+        }
+    }
+}
