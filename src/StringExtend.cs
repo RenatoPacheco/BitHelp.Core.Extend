5adda84e4e98f88ceed9bb4891c85ef6a4cfcf91
Renato Pacheco <renato.pacheco@adm.unip.br>
2023-10-26 19:12:52 -0300

    refactor: Adding DoesCultureExist and NonCultureExist to class string

---
+++ src/StringExtend.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+
+namespace BitHelp.Core.Extend
+{
+    public static class StringExtend
+    {
+        public static bool DoesCultureExist(this string source)
+        {
+            return !string.IsNullOrEmpty(source) && CultureInfo.GetCultures(CultureTypes.AllCultures).Any(
+                culture => string.Equals(culture.Name, source, StringComparison.CurrentCultureIgnoreCase));
+        }
+
+        public static bool NonCultureExist(this string source)
+        {
+            return string.IsNullOrEmpty(source) || !CultureInfo.GetCultures(CultureTypes.AllCultures).Any(
+                culture => string.Equals(culture.Name, source, StringComparison.CurrentCultureIgnoreCase));
+        }
+    }
+}
---
+++ test/StringExtendTest/NonCultureExistTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BitHelp.Core.Extend.Test.StringExtendTest
+{
+    public class NonCultureExistTest
+    {
+        [Theory]
+        [InlineData("pt-BR")]
+        [InlineData("en-US")]
+        [InlineData("PT-BR")]
+        public void Check_value_not_is_culture_invalid(string input)
+        {
+            Assert.False(input.NonCultureExist(), $"Expected: True \nActual: False \nValue: {input}");
+            Assert.Equal(input.ToLower(), (new CultureInfo(input)).ToString().ToLower());
+        }
+
+        [Theory]
+        [InlineData("PT-US")]
+        [InlineData("pt-US")]
+        [InlineData("  pt-BR  ")]
+        [InlineData("     ")]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Check_value__not_is_culture_valid(string input)
+        {
+            Assert.True(input.NonCultureExist(), $"Expected: False \nActual: True \nValue: {input}");
+        }
+    }
+}
